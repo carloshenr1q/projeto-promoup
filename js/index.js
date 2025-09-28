4807bba3ceb1664a6ab2b83cf944a8e67bd95f12
Carlos Henrique <carloshenr1q.dev@gmail.com>
2025-09-28 00:31:20 -0300

    feat/style/script: Add imagens, novas boxs e pondo estilo nelas e responsividade para tablet/mobile, ocorreu a necessidade de pôr JS em div.background para tela menores para rolagem lateral das imagens

--- js/index.js
+++ js/index.js
@@ -17,3 +17,36 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 // ======================================================================
+
+
+
+document.querySelectorAll('.background').forEach(container => {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  container.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener('mouseleave', () => {
+    isDown = false;
+  });
+
+  container.addEventListener('mouseup', () => {
+    isDown = false;
+  });
+
+  container.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 0.9; // fator de velocidade
+    container.scrollLeft = scrollLeft - walk;
+  });
+});
+
+
+
