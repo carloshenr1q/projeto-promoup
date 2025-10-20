7405272625d3bae5433efd9d0e2108827c009b9f
Carlos Henrique <carloshenr1q.dev@gmail.com>
2025-10-20 14:25:19 -0300

    feat: Add tags meta, script / Organizando pastas, renomeando arquivos, e add comentários.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 // AÇÃO E ANIMAÇÃO PARA APARECER MENU
-// ======================================================================
 document.addEventListener("DOMContentLoaded", () => {
-  const botaoMenu = document.querySelector(".botao-menu");
+  const botaoMenu = document.querySelector("div.menu-mobile > i");
   const nav = document.querySelector("header nav");
 
   botaoMenu.addEventListener("click", (e) => {
     e.stopPropagation(); // impede que o clique no botão feche imediatamente
     nav.classList.toggle("ativo");
+    botaoMenu.classList.toggle("menu-ativo");
   });
 
   // Fecha o menu se clicar fora dele
   document.addEventListener("click", (e) => {
     if (nav.classList.contains("ativo") && !nav.contains(e.target) && !botaoMenu.contains(e.target)) {
       nav.classList.remove("ativo");
+      botaoMenu.classList.remove("menu-ativo");
     }
   });
 });
